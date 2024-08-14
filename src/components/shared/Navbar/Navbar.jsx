596873625518e8f2a18bb10c1203be1e2450a14d
mahfuj926 <mahfujrahman.jewel@gmail.com>
2024-08-14 21:01:08 +0600

    Added Navbar

---
+++ src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="bg-[#a29bfe] ">
+            <div className="container mx-auto py-6">
+                <ul className="flex justify-between items-center ">
+                    <li className="text-4xl font-bold">Star Plus</li>
+                    <li>Login</li>
+                    <li>Logout</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;

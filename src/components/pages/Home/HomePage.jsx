636287249e23ade46cb33f70017964b201268bf9
mahfuj926 <mahfujrahman.jewel@gmail.com>
2024-08-16 21:13:00 +0600

    Added simple pagination to test

--- src/components/pages/Home/HomePage.jsx
+++ src/components/pages/Home/HomePage.jsx
@@ -1,11 +1,10 @@
-import Banner from "./Banner/Banner";
+
 import ProductCards from "./ProductCards/ProductCards";
 
 
 const HomePage = () => {
     return (
         <div>
-            <Banner></Banner>
             <ProductCards></ProductCards>
         </div>
     );
